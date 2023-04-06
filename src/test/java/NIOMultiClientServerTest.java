325e33a5409fafbcb750d655e688491d55fcf6bd
jwp345 <jwp135@naver.com>
2023-04-06 13:33:17 +0900

    1차 해결: 출력 전용 스레드를 생성해서 읽기 이벤트를 수신하게끔 만듦.

--- src/test/java/NIOMultiClientServerTest.java
+++ src/test/java/NIOMultiClientServerTest.java
@@ -1,12 +1,10 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -37,9 +35,39 @@ class NIOMultiClientServerTest {
     public void testSocketChannelClient() throws IOException, InterruptedException {
         List<CompletableFuture<Void>> futures = new ArrayList<>();
         Process server = startServer();
+        new Thread(() -> {
+            /*ReadableByteChannel channel = Channels.newChannel(server.getInputStream());
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (true) {
+                int bytes;
+                try {
+                    if ((bytes = channel.read(buffer)) <= 0) continue;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                buffer.flip();
+                String str = new String(buffer.array(), 0, bytes);
+                System.out.println(str);
+                buffer.clear();
+            }
+             */
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int bytes;
+                try {
+                    if((bytes = server.getInputStream().read(buffer)) <= 0) continue;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                String str = new String(buffer, 0, bytes);
+                System.out.println(str);
+            }
+        }).start(); // 출력 전용 스레드 생성
+
         Thread.sleep(1000); // 서버가 실행될 때까지 약간의 텀이 필요함. 없으면 서버가 켜지기 전에 connection 시도로 connection refused exception 발생.
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             futures.add(CompletableFuture.runAsync(() -> {
                 try {
                     startClient(numClients.getAndDecrement()); // 왜 DecrementAndGet()메소드는 일괄적인 값을 리턴할까..?
@@ -53,16 +81,6 @@ class NIOMultiClientServerTest {
         Thread.sleep(1000); // 마찬가지로 연결 종료시까지 시간이 필요함.
 //        server.waitFor();
         server.destroy();
-
-        copy(server.getInputStream(), System.out);
-    }
-
-    private void copy(InputStream input, OutputStream output) throws IOException {
-        byte[] buffer = new byte[1024];
-        int n = 0;
-        while ((n = input.read(buffer)) != -1) {
-            output.write(buffer, 0, n);
-        }
     }
 
     private void startClient(int num) throws IOException {
@@ -86,10 +104,5 @@ class NIOMultiClientServerTest {
 //        socketChannel.shutdownOutput();
     }
 
-//    @AfterEach
-//    void cleanUp() {
-//        server.destroy();
-//
-//    }
 
 }
