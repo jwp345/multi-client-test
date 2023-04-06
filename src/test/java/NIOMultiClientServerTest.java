f4524cd8c98976fc9e58c129c3133fa146d02d94
jwp345 <jwp135@naver.com>
2023-04-06 16:29:46 +0900

    nio Pipe 클래스와 Selector을 이용하여 이벤트 기반 논블로킹하게 클라이언트를 수정

--- src/test/java/NIOMultiClientServerTest.java
+++ src/test/java/NIOMultiClientServerTest.java
@@ -33,44 +33,17 @@ class NIOMultiClientServerTest {
 
     @Test
     public void testSocketChannelClient() throws IOException, InterruptedException {
+        Pipe pipe = Pipe.open();
         List<CompletableFuture<Void>> futures = new ArrayList<>();
         Process server = startServer();
-        new Thread(() -> {
-            /*ReadableByteChannel channel = Channels.newChannel(server.getInputStream());
-            ByteBuffer buffer = ByteBuffer.allocate(1024);
-
-            while (true) {
-                int bytes;
-                try {
-                    if ((bytes = channel.read(buffer)) <= 0) continue;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                buffer.flip();
-                String str = new String(buffer.array(), 0, bytes);
-                System.out.println(str);
-                buffer.clear();
-            }
-             */
-            byte[] buffer = new byte[1024];
-            while (true) {
-                int bytes;
-                try {
-                    if((bytes = server.getInputStream().read(buffer)) <= 0) continue;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                String str = new String(buffer, 0, bytes);
-                System.out.println(str);
-            }
-        }).start(); // 출력 전용 스레드 생성
+        InputStream inputStream = server.getInputStream();
 
         Thread.sleep(1000); // 서버가 실행될 때까지 약간의 텀이 필요함. 없으면 서버가 켜지기 전에 connection 시도로 connection refused exception 발생.
 
         for (int i = 0; i < 10; i++) {
             futures.add(CompletableFuture.runAsync(() -> {
                 try {
-                    startClient(numClients.getAndDecrement()); // 왜 DecrementAndGet()메소드는 일괄적인 값을 리턴할까..?
+                    startClient(numClients.getAndDecrement(), pipe, inputStream); // 왜 DecrementAndGet()메소드는 일괄적인 값을 리턴할까..?
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -79,29 +52,111 @@ class NIOMultiClientServerTest {
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         Thread.sleep(1000); // 마찬가지로 연결 종료시까지 시간이 필요함.
+
 //        server.waitFor();
         server.destroy();
     }
 
-    private void startClient(int num) throws IOException {
+    private void startClient(int num, Pipe pipe, InputStream inputStream) throws IOException {
+        Selector selector = Selector.open();
+        Pipe.SourceChannel sourceChannel = pipe.source();
+        Pipe.SinkChannel sinkChannel = pipe.sink();
+        sourceChannel.configureBlocking(false);
+        sinkChannel.configureBlocking(false);
+        sourceChannel.register(selector, SelectionKey.OP_READ); // 읽기 이벤트 등록
+        sinkChannel.register(selector, SelectionKey.OP_WRITE); // 쓰기 이벤트 등록
         SocketChannel socketChannel = SocketChannel.open();
-//        socketChannel.configureBlocking(false); // 응답을 받아오려면 논블로킹으로 보낼 수 없다. 응답이 오기까지 블로킹 되야한다.
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, SelectionKey.OP_CONNECT); // 연결 이벤트 등록
         socketChannel.connect(new InetSocketAddress("localhost", 1234));
-
         ByteBuffer buffer = ByteBuffer.allocate(1024);
-        String message = "Hello, NIO Server!" + num;
-        buffer.put(message.getBytes());
-        buffer.flip();
-        socketChannel.write(buffer);
 
-        buffer.clear();
-        int bytesRead = socketChannel.read(buffer);
-        String response = new String(buffer.array(), 0, bytesRead);
-        System.out.println("Response : " + response);
-        assertEquals("Hello, NIO Client!" + num, response.trim());
-        socketChannel.close();
-//        socketChannel.shutdownInput();
-//        socketChannel.shutdownOutput();
+        loop : while(true) {
+            selector.select();
+
+            // 발생한 이벤트를 Set에 저장
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                if(key.isConnectable()) {
+                    connectSocket(socketChannel, selector, num);
+                }
+                if(key.isReadable()) {
+                    if(isEndReadByChannel(key, num, buffer)) {
+                        break loop;
+                    }
+                }
+                if(key.isWritable()) {
+                    writeByChannel(key, inputStream, buffer);
+                }
+                keyIterator.remove();
+            }
+        }
+//        sourceChannel.close();
+//        sinkChannel.close();
+    }
+
+    private void connectSocket(SocketChannel socketChannel, Selector selector, int num) throws IOException {
+        while (!socketChannel.finishConnect()) {}
+        ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+        String message = "Hello, NIO Server!" + num;
+        writeBuffer.put(message.getBytes());
+        writeBuffer.flip();
+        socketChannel.register(selector, SelectionKey.OP_WRITE, writeBuffer);
+    }
+
+    private void writeByChannel(SelectionKey key, InputStream inputStream, ByteBuffer buffer) throws IOException {
+        if(key.channel() instanceof SocketChannel) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            ByteBuffer writeBuffer = (ByteBuffer) key.attachment();
+            socketChannel.write(writeBuffer);
+//            socketChannel.register(selector, SelectionKey.OP_READ); // 결과는 같다.
+            key.interestOps(SelectionKey.OP_READ);
+        }
+        if(key.channel() instanceof Pipe.SinkChannel) {
+            Pipe.SinkChannel sinkChannel = (Pipe.SinkChannel) key.channel();
+            int bytesRead = inputStream.read(buffer.array());
+            if(bytesRead == -1) {
+                return;
+            } if(bytesRead > 0) {
+                buffer.position(bytesRead); // 이게 뭐지?
+                buffer.flip();
+                sinkChannel.write(buffer);
+                buffer.compact(); // 이것도 머냐?
+                buffer.clear();
+            }
+        }
+    }
+
+    private boolean isEndReadByChannel(SelectionKey key, int num, ByteBuffer buffer) throws IOException {
+        int bytesRead;
+        if(key.channel() instanceof SocketChannel) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            bytesRead = socketChannel.read(buffer);
+            String response = new String(buffer.array(), 0, bytesRead);
+            System.out.println("Response : " + response);
+            assertEquals("Hello, NIO Client!" + num, response.trim());
+            socketChannel.close();
+            return true; // 명시적으로 종료하기 위해 지금은 boolean 쓰지만 변경 예정
+        }
+        if(key.channel() instanceof Pipe.SourceChannel) {
+            Pipe.SourceChannel sourceChannel = (Pipe.SourceChannel) key.channel();
+            bytesRead = sourceChannel.read(buffer);
+
+            if (bytesRead == -1) {
+                return false;
+            }
+            if(bytesRead > 0) {
+                buffer.flip();
+                byte[] bytes = new byte[bytesRead];
+                buffer.get(bytes);
+                System.out.println(new String(bytes));
+                buffer.clear();
+            }
+        }
+        return false;
     }
 
 
